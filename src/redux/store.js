10aadde8ca9e68602220597b23685c97141d369e
hugues0 <ntwari.hugues@gmail.com>
2021-07-31 22:20:19 +0200

    reducer bug fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,9 @@ import rootReducer from '/root-reducer';
 
 const middlewares = [reduxThunk]
 
-
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger)
+}
 
 const store = createStore(rootReducer,applyMiddleware{...middlewares})
 
