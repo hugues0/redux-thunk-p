837b05404eaae8dfac2bbbadfa387c1d4576308f
hugues0 <ntwari.hugues@gmail.com>
2021-08-01 22:12:44 +0200

    mateial UI elements

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import {createStore,applyMiddleware} from 'redux'
 import logger from 'redux-logger'
 import reduxThunk from 'redux-thunk'
-import rootReducer from '/root-reducer';
+import rootReducer from './root-reducer';
 
 const middlewares = [reduxThunk]
 
@@ -9,6 +9,6 @@ if(process.env.NODE_ENV === 'development'){
     middlewares.push(logger)
 }
 
-const store = createStore(rootReducer,applyMiddleware{...middlewares})
+const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export default store
